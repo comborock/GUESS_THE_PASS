a56fc62874c7186093c750a062baed1298043006
Bod <53569898+comborock@users.noreply.github.com>
2024-09-27 16:56:03 +0200

    Add files via upload

---
+++ high_low_letters.py
@@ -0,0 +1,28 @@
+import string
+high = string.ascii_lowercase
+#abcdefghijklmnopqrstuvwxyz
+high_and_low = "bdfghjklpqty"
+high_let = list("bdfhklt")
+low_let = list("gjpqy")
+middle_let = list("aceimnorsuvwxz")
+HIGH_LET = string.ascii_uppercase
+NUM = string.digits
+SYM = string.punctuation
+print(SYM)
+
+string = input("PUT IN TEXT:")
+str2 = ""
+for i in string:
+    if i in high_let:
+        str2 += "H"
+    if i in low_let:
+        str2 += "L"
+    if i in middle_let:
+        str2 += "M"
+    if i in HIGH_LET:
+        str2 += "C"
+    if i in NUM:
+        str2 += "N"
+    if i in SYM:
+        str2 += "S"
+print(str2)
